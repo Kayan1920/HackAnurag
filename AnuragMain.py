0d41f514c4d6f4218e8347ba60ad71b551bebbeb
Kayan19 <35771257+Kayan1920@users.noreply.github.com>
2024-05-22 01:14:31 +0530

    Update AnuragMain.py

--- AnuragMain.py
+++ AnuragMain.py
@@ -2,15 +2,63 @@ import streamlit as st
 import webbrowser
 import hashlib
 import time
+import base64
+import streamlit as st
+import requests
+import pandas as pd
 
-# Page title
-st.title("CTF Challenge")
+MAX_ATTEMPTS = 10
+
+try:
+    num_attempts = pd.read_csv("NumTries.csv")
+except:
+    num_attempts = pd.DataFrame([['Anurag', 0]], columns = ['Name', 'NumTries'])
+    num_attempts.to_csv("NumTries.csv", index=False)
+
+def set_gif_background(gif_url):
+    gif_data = base64.b64encode(requests.get(gif_url).content)
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background: url(data:image/gif;base64,{gif_data.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
+gif_url = 'https://i.gifer.com/3HeZ.gif' # replace with your gif url
+set_gif_background(gif_url)
+
+st.markdown('''<h1 style="color:white;">CTF Challenge</h1>''', unsafe_allow_html=True)
+
+st.write("$$\\color{white}{\\text{Guess this four digit flag to reveal our intentions. - Courtesy of Kayan and Ayushi (tm)}}$$")
+st.markdown("<p style='color:white'>Clue #1 : Thou hath already posses this key within thy mobile.</p>", unsafe_allow_html=True)
+st.markdown("<p style='color:white'>Clue #2 : Despite numerous awards and accolades perhaps a recent and novel accomplishment holds the true secret.</p>", unsafe_allow_html=True)
+st.markdown("<p style='color:white'>Clue #3 : Image</p>", unsafe_allow_html=True)
+st.image("unknownLogo.jpeg", caption="The image we were just talking about.")
+
+
+with st.popover("Get a hint -> Lose 5 pts"):
+    st.error("Really Anurag? A hint?")
+    st.image("Judgement.jpeg",caption="Our face when you ask for hints.")
 
 # Text input for flag
 flag = st.text_input("Enter the flag:")
 
-# Check if flag is correct
-if st.button("Submit"):
+
+def check_flag():
+    global flag, num_attempts
+
+    num_attempts = pd.read_csv("NumTries.csv")
+    if num_attempts.iloc[0, 1] >= MAX_ATTEMPTS:
+        st.error("You have exceeded the maximum number of tries. Contact Kayan for further assistance.")
+    
+    num_attempts.iloc[0, 1] += 1
+    num_attempts.to_csv("NumTries.csv", index=False)
+
     hash = hashlib.md5(flag.encode())
     hash = hash.hexdigest()
     if hash == "6346dc723395e1ee8ef57f4883be4cb4":
@@ -22,3 +70,8 @@ if st.button("Submit"):
     else:
         x = st.error("Noob")
         st.image("AnuragFail.JPG", caption="Anurag is a noob")
+
+# Check if flag is correct
+if st.button("Submit"):
+    check_flag()
+
